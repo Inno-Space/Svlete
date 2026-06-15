53786ad088afdbdd1c62ad7c967785e891136581
i-i <i-i@seraku.co.jp>
2026-06-15 18:01:48 +0900

    3강까지 함, 이제 4강 하면 됌 Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	let { children } = $props();
+</script>
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">紹介</a>
+</nav>
+
+<hr />
+{#if children}
+	{@render children()}
+{/if}
+<hr />
+
+<footer>
+	2026年Svelte練習ページ作成する。All right reserved
+</footer>
+
+<style>
+	nav {
+		padding: 1rem;
+		background-color: #f4f4f4;
+	}
+
+	nav a {
+		margin-right: 10px;
+		text-decoration: none;
+		font-weight: bold;
+		color: #ff3e00;
+	}
+
+	footer {
+		text-align: center;
+		font-size: 0.8rem;
+		color: gray;
+	}
+</style>
